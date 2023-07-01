59cd10d169d042623ddf8f59f9d4947ef21b4936
Aditya Saini <Adityasaini10012001@outlook.com>
2023-07-01 16:19:27 +0530

    added already installed

--- install_gprmax.py
+++ install_gprmax.py
@@ -3,40 +3,82 @@ import os
 import subprocess
 import platform
 
+
+
+def check_installed_software(software):
+    try:
+        subprocess.check_output([software, '--version'])
+        return True
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        return False
+
+def upgrade_software(software, upgrade_command):
+    try:
+        subprocess.check_call(upgrade_command, shell=True)
+        print(f"{software} upgraded successfully.")
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        print(f"Failed to upgrade {software}.")
+        print(f"Please manually upgrade {software}.")
+
+def install_software(software, install_command):
+    try:
+        subprocess.check_call(install_command, shell=True)
+        print(f"{software} installed successfully.")
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        print(f"Failed to install {software}.")
+        print(f"Please manually install {software}.")
+
+
+# starting of install_gprMax() function
+
 def install_gprMax():
     # Step 1: Install Miniconda (Windows, Ubuntu, and macOS)
-    if platform.system() == "Windows":
+    
+    if check_installed_software('conda'):
+        print("Miniconda is already installed.Upgrading to the latest version...")
+        current_platform = platform.system()
+        if current_platform == 'Windows':
+            subprocess.call(['conda', 'update', '-n', 'base', '-c', 'defaults', '--all', '-y'])
+  # upgrade_software('conda', 'choco install miniconda3 -y')
+        elif current_platform == 'Darwin':
+            subprocess.call(['conda', 'update', '-n', 'base', '-c', 'defaults', '--all', '-y'])
+            # upgrade_software('conda', 'brew install miniconda')
+        elif current_platform == 'Linux':
+            subprocess.call(['conda', 'update', '-n', 'base', '-c', 'defaults', '--all', '-y'])
+            
+          
+    else:
+        if platform.system() == "Windows":
+            
         # Download Miniconda for Windows
-        miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-        os.system(f"powershell -Command \"Invoke-WebRequest '{miniconda_url}' -OutFile 'Miniconda3-latest-Windows-x86_64.exe'\"")
-        os.system("start Miniconda3-latest-Windows-x86_64.exe")
+            miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+            os.system(f"powershell -Command \"Invoke-WebRequest '{miniconda_url}' -OutFile 'Miniconda3-latest-Windows-x86_64.exe'\"")
+            os.system("start Miniconda3-latest-Windows-x86_64.exe")
         # Wait for the Miniconda installation to complete
-        input("Press Enter when Miniconda installation is complete...")
-    elif platform.system() == "Linux":
-        # Download Miniconda for Linux
-        miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-        os.system(f"wget {miniconda_url}")
-        os.system("chmod +x Miniconda3-latest-Linux-x86_64.sh")
-        os.system("./Miniconda3-latest-Linux-x86_64.sh")
+            input("Press Enter when Miniconda installation is complete...")
         
-    elif platform.system() == "Darwin":
-        # Download Miniconda for macOS
-        if platform.machine().endswith("64"):
-            miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
-        elif platform.machine().endswith("arm64"):
-            miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh"
         
-        # else:
-        #     raise Exception("Unsupported macOS architecture.")
+        elif platform.system() == "Linux":
+            
+            miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+            os.system(f"wget {miniconda_url}")
+            os.system("chmod +x Miniconda3-latest-Linux-x86_64.sh")
+            os.system("./Miniconda3-latest-Linux-x86_64.sh")
+        
+        elif platform.system() == "Darwin":
+            
+            if platform.machine().endswith("64"):
+                miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+            elif platform.machine().endswith("arm64"):
+                miniconda_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh"
+            os.system(f"curl -O {miniconda_url}")
+            os.system("chmod +x " + os.path.basename(miniconda_url))
+            os.system(f"./{os.path.basename(miniconda_url)}")
 
-        os.system(f"curl -O {miniconda_url}")
-        os.system("chmod +x " + os.path.basename(miniconda_url))
-        os.system(f"./{os.path.basename(miniconda_url)}")
-
-    continue_install = input("Continue installation? Enter 'yes' to continue: ")
-    if continue_install.lower() != "yes":
-        print("Installation aborted.")
-        return
+        continue_install = input("Continue installation? Enter 'yes' to continue: ")
+        if continue_install.lower() != "yes":
+            print("Installation aborted.")
+            return
 
     # Step 2: Install Git and clone gprMax repository
     result = subprocess.run(["conda", "update", "conda", "-y"])
